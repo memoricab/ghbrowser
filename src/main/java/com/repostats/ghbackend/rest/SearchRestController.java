e80928c0759073399c641ea85ab2782c92607e79
mehmet celen <memo@celenmeh-PC.lan>
2020-01-28 18:55:40 +0200

    Added blank username control to searchUserByUsername

--- src/main/java/com/repostats/ghbackend/rest/SearchRestController.java
+++ src/main/java/com/repostats/ghbackend/rest/SearchRestController.java
@@ -4,6 +4,8 @@ import com.repostats.ghbackend.dto.SearchDTO;
 import com.repostats.ghbackend.dto.UserDTO;
 import com.repostats.ghbackend.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +23,11 @@ public class SearchRestController {
 
     @GetMapping
     @PreAuthorize("hasRole('USER')")
-    public UserDTO searchUserByUsername(@RequestParam String username) {
-        return searchService.searchByGithubUsername(username);
+    public ResponseEntity searchUserByUsername(@RequestParam String username) {
+        if(username.isBlank()){
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<UserDTO>(searchService.searchByGithubUsername(username),HttpStatus.OK);
     }
 
     @GetMapping("/")
