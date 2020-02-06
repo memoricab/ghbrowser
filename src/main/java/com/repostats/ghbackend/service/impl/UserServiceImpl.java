53abc1bbf1b2ea507c54809eb5a09d85d8cbd1ff
mehmet celen <memo@celenmeh-PC.lan>
2020-02-06 18:16:32 +0200

    Make service impl package-private

--- src/main/java/com/repostats/ghbackend/service/impl/UserServiceImpl.java
+++ src/main/java/com/repostats/ghbackend/service/impl/UserServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserServiceImpl implements UserService {
+class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
